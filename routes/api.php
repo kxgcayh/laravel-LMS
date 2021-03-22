8866db43e3c3a555e517e0456f8870e48cdf37c5
Kautsar Al Bana <kautsaralbana@hotmail.com>
2021-03-22 12:22:57 +0700

    Rest Zoom API Integration

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Zoom\MeetingController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Get list of meetings.
+Route::get('/meetings', [MeetingController::class, 'list']);
+
+// Create meeting room using topic, agenda, start_time.
+Route::post('/meetings', [MeetingController::class, 'create']);
+
+// Get information of the meeting room by ID.
+Route::get('/meetings/{id}', [MeetingController::class, 'get'])->where('id', '[0-9]+');
+Route::patch('/meetings/{id}', [MeetingController::class, 'update'])->where('id', '[0-9]+');
+Route::delete('/meetings/{id}', [MeetingController::class, 'delete'])->where('id', '[0-9]+');
